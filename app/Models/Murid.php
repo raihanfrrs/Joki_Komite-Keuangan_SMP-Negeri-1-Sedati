a85ab1801410332a54f9e3d952df36e12e0c5d1f
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2024-06-03 08:28:46 +0700

    feat: murid

---
+++ app/Models/Murid.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Murid extends Model
+{
+    use HasFactory;
+
+    protected $keyType = "string";
+    public $incrementing = false;
+    protected $primaryKey = 'id';
+    protected $fillable = [
+        'id',
+        'wali_murid_id',
+        'name',
+        'wali_murid'
+    ];
+
+    public function wali_murid()
+    {
+        return $this->belongsTo(WaliMurid::class);
+    }
+}
