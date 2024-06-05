c6454b45fc02a3b0053665f66fc0049603de76ab
Mohamad Raihan Farras <rehanfarras76@gmail.com>
2024-06-05 13:25:28 +0700

    feat: admin dashboard

--- app/Http/Controllers/LayoutController.php
+++ app/Http/Controllers/LayoutController.php
@@ -5,14 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Repositories\NewsRepository;
 use Illuminate\Support\Facades\Auth;
+use App\Repositories\ClassRepository;
+use App\Repositories\MuridRepository;
+use App\Repositories\PaymentRepository;
 
 class LayoutController extends Controller
 {
-    protected $news;
+    protected $news, $payment, $class, $murid;
 
-    public function __construct(NewsRepository $newsRepository)
+    public function __construct(NewsRepository $newsRepository, PaymentRepository $paymentRepository, ClassRepository $classRepository, MuridRepository $muridRepository)
     {
         $this->news = $newsRepository;
+        $this->payment = $paymentRepository;
+        $this->class = $classRepository;
+        $this->murid = $muridRepository;
     }
 
     public function index()
@@ -23,7 +29,12 @@ class LayoutController extends Controller
 
         if (Auth::check() && auth()->user()->level == 'admin') {
             return view('pages.admin.dashboard.index', [
-                'news' => $this->news->getAllNewsByLimit(4)
+                'news' => $this->news->getAllNewsByLimit(4),
+                'payment' => $this->payment->getAllPaymentsByApproveStatus(),
+                'totalPaymentThisMonth' => $this->payment->getTotalPaymentThisMonth(),
+                'totalNewsThisMonth' => $this->news->getTotalNewsThisMonth(),
+                'totalClass' => $this->class->getAllClasses()->count(),
+                'totalStudent' => $this->murid->getAllMurid()->count(),
             ]);
         } elseif (Auth::check() && auth()->user()->level == 'wali murid') {
             return view('pages.wali_murid.dashboard.index', [
